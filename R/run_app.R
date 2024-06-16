04c595177597f5ad108d63b138e71c2d23b8e5a3
brandonerose <brandon.ed.rose@gmail.com>
2024-06-16 07:14:23 -0400

    dev

--- R/run_app.R
+++ R/run_app.R
@@ -1,11 +1,11 @@
 #' @import RosyUtils
+#' @title run_app
 #' @param ... arguments to pass to golem_opts.
 #' See `?golem::get_golem_options` for more details.
 #' @inheritParams shiny::shinyApp
-#'
-#' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
+#' @export
 run_app <- function(
     onStart = NULL,
     options = list(),
