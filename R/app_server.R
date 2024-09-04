2cae2a6942ab8356376dba45ade817e37c36808c
brandonerose <brandon.ed.rose@gmail.com>
2024-09-04 10:23:04 -0400

    dev

--- R/app_server.R
+++ R/app_server.R
@@ -4,10 +4,10 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
-  output$REDCap_diagram_vis <- DiagrammeR::renderGrViz({
+  output$REDCap_diagram_diag <- DiagrammeR::renderGrViz({
     DiagrammeR::grViz(DiagrammeR::generate_dot(REDCap_diagram(DB, type = "DiagrammeR",render = F)))
   })
-  output$REDCap_diagram_diag <- visNetwork::renderVisNetwork({
+  output$REDCap_diagram_vis <- visNetwork::renderVisNetwork({
     REDCap_diagram(DB, type = "visNetwork",render = T)
   })
 }
